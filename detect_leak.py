c4d3a15abdff4e2bf4c26e81a30ff177bc583928
Harry Tung <honeytung@gmail.com>
2025-03-28 16:18:12 -0700

    Code cleanup and add comments

--- detect_leak.py
+++ detect_leak.py
@@ -39,6 +39,7 @@ if __name__ == "__main__":
     while True:
         try:
             frame = grabber.grab()
+            annotated_frame = frame.copy()
 
             if config.enable_motion_detection and not motion_detector.motion_detected(frame):
                 logger.info(f"No significant motion detected in frame {current_frame_num}, skipping frame.")
@@ -46,16 +47,18 @@ if __name__ == "__main__":
                 continue
 
         except Exception as e:
-            logger.warning(f"Error grabbing frame. Possible end of file: {e}", exc_info=True)
+            logger.warning(f"Cannot grab next frame. Possible end of file: {e}", exc_info=True)
             break
 
         try:
+            # Send the frame to the binary leak detector to check if there are any leaks in the frame
             iq_detect_leaks = gl.submit_image_query(detector=detect_leaks, image=frame)
 
             if iq_detect_leaks.confidence_threshold < iq_detect_leaks.result.confidence and iq_detect_leaks.result.label == "YES":
                 logger.debug(f"Leak detected with confidence: {iq_detect_leaks.result.confidence}")
                 logger.debug("Sending frame to counting detector")
 
+                # Send the frame to the count leak detector to get the ROIs of the all the leaks in the frame
                 iq_count_leaks = gl.submit_image_query(detector=count_leaks, image=frame)
 
                 if iq_count_leaks.confidence_threshold < iq_count_leaks.result.confidence:
@@ -65,7 +68,7 @@ if __name__ == "__main__":
 
                     if leak_rois is not None:
                         for roi in leak_rois:
-                            # For each detected leak, send the ROI to the classifier to classify what kind of leak it is
+                            # For each detected leak, send the ROI to the classifier to classify what type of leak it is
                             # Expand the ROI to include some context around the leak, 5% of each side
 
                             (h, w) = frame.shape[:2]
@@ -87,15 +90,22 @@ if __name__ == "__main__":
 
                             if iq_classify_leaks.confidence_threshold < iq_classify_leaks.result.confidence:
                                 logger.debug(f"Leak classified with confidence: {iq_classify_leaks.result.confidence}")
+                                logger.info(f"Frame {current_frame_num}: Leak detected and classified as: {iq_classify_leaks.result.label}")
 
-                                # Draw the bounding box on the original frame
-                                cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
+                                # Draw the bounding box and the class on the annotated frame
+                                cv2.rectangle(annotated_frame, (left, top), (right, bottom), (0, 255, 0), 2)
                                 cv2.putText(
-                                    frame, f"Leak: {iq_classify_leaks.result.label}", (left, top - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2
+                                    annotated_frame,
+                                    f"Leak: {iq_classify_leaks.result.label}",
+                                    (left, top - 10),
+                                    cv2.FONT_HERSHEY_SIMPLEX,
+                                    0.5,
+                                    (0, 255, 0),
+                                    2,
                                 )
 
             # Show the frame with the detected leaks
-            cv2.imshow("Frame", frame)
+            cv2.imshow("Frame", annotated_frame)
             if cv2.waitKey(1) & 0xFF == ord("q"):
                 break
 
